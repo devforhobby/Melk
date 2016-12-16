e89fe1dbc4eb0b4dfa232d3bd278cf300e849bc4
hamedrajabpour <hamedrajabpoor724@gmail.com>
2016-12-16 20:55:27 +0330

    Good Login Activity

--- app/src/main/java/io/github/hamedrajabpour/melk/MainActivity.java
+++ app/src/main/java/io/github/hamedrajabpour/melk/MainActivity.java
@@ -13,6 +13,5 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_menu_black_24dp);
     }
 }
